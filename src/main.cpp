b3627cf28ab05055b1ff8e326e101f7648cbd055
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-01-27 08:59:38 -0400

    Necessidade de refatoração dado a exclusão das classes para teste de compilação

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,4 @@
-#include <iostream>
-#include "domain/Purchase.h"
 
 int main() {
-    auto purchase = Purchase::createSummary(
-        "Supermercado X",
-        123.45
-    );
-
-    std::cout << purchase.location() << " - "
-              << purchase.total() << "\n";
-
     return 0;
 }
