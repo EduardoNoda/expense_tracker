2587ca090821d91fc389a1bd1be17d4e2b410d14
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-01-28 06:33:30 -0400

    refactor(MoneyManager.cpp): variável não condizia com o inicializador

--- src/MoneyManager.cpp
+++ src/MoneyManager.cpp
@@ -3,11 +3,11 @@
 #include <iomanip>
 #include <sstream>
 
-monataryValue::monataryValue() : value(0) {}
-monataryValue::monataryValue(long long value) : value(value) {}
+monataryValue::monataryValue() : valueInCents(0) {}
+monataryValue::monataryValue(long long value) : valueInCents(value) {}
 
 std::string monataryValue::toString() const {
     std::stringstream valueFormatted;
-    valueFormatted << "R$" << (value) << "." << std::setw(2) << std::setfill('0') << std::abs(value % 100);
+    valueFormatted << "R$" << (valueInCents) << "." << std::setw(2) << std::setfill('0') << std::abs(valueInCents % 100);
     return valueFormatted.str();
 }
