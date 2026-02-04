b4732dbba2601fdf48579f873e8654bb3ede4d56
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-04 12:02:14 -0400

    refactor: alteracao no nome income->revenue para adaptar-se ao padrao do dominio e necessidade de mudancas nas regras do expense

--- src/infrastructure/schema.sql
+++ src/infrastructure/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE payment_methods (
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE incomes (
+CREATE TABLE revenues (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     amount_cents INTEGER NOT NULL CHECK (amount_cents > 0),
     date TEXT NOT NULL, -- ISO 8601
@@ -19,12 +19,14 @@ CREATE TABLE incomes (
 
 CREATE TABLE expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    revenue_id INTEGER NOT NULL,
     amount_cents INTEGER NOT NULL CHECK (amount_cents > 0),
-    date TEXT NOT NULL, -- ISO 8601
+    date TEXT NOT NULL,
     category_id INTEGER NOT NULL,
     payment_method_id INTEGER NOT NULL,
     created_at TEXT NOT NULL DEFAULT (datetime('now')),
 
+    FOREIGN KEY (revenue_id) REFERENCES revenues(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT,
     FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id) ON DELETE RESTRICT
 );
