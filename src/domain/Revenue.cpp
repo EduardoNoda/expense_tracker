c37fe5a50ed07fa6a5cd6aeb6d4fe195d8350320
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-04 12:00:37 -0400

    refactor: necessidade de mudancas por conflito com o objetivo real do sistema

--- src/domain/Revenue.cpp
+++ src/domain/Revenue.cpp
@@ -1,11 +1,30 @@
 #include "Revenue.h"
+#include "Expense.h"
 #include "Money.h"
+#include <stdexcept>
 
-Revenue::Revenue(
-    Money amount, 
-    Date date) : 
-    amount(amount), 
-    date(date){}
-
+Revenue::Revenue(int id, Money amount, Date date) 
+    : id(id), amount(amount), date(date){
+    if(id <= 0){
+        throw new std::invalid_argument("Id precisa ser positivo");
+    }
+}
+int Revenue::getId() const {return id;}
 const Money& Revenue::getAmount() const {return amount;}
 const Date& Revenue::getDate() const {return date;}
+
+void Revenue::addExpense(const Expense& expense) {
+    expenses.push_back(expense);
+}
+
+Money Revenue::totalExpenses() const{
+    Money total(0);
+
+    for(const auto& e : expenses) total = total + e.getAmount();
+
+    return total;
+}
+
+Money Revenue::remaining() const{
+    return amount - totalExpenses();
+}
--- src/domain/Revenue.h
+++ src/domain/Revenue.h
@@ -2,18 +2,26 @@
 
 #include "Money.h"
 #include "Date.h"
+#include "Expense.h"
+#include <vector>
 
 class Revenue {
 public:
-    Revenue (
-        Money amount,
-        Date date
-    );
+    Revenue (int id, Money amaout, Date date);
 
+    int getId() const;
     const Money& getAmount() const;
     const Date& getDate() const;
 
+    void addExpense(const Expense&);
+
+    Money totalExpenses() const;
+    Money remaining() const;
+
 private:
+    int id;
     Money amount;
     Date date;
+
+    std::vector<Expense> expenses;
 };
