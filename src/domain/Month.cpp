04abeb67175c2f2d571ceff5083caf0279da9832
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-04 12:02:55 -0400

    feat: criado a classe month para controle de receitas do sistema

---
+++ src/domain/Month.cpp
@@ -0,0 +1,36 @@
+#include "Month.h"
+#include "Money.h"
+#include "Revenue.h"
+
+Month::Month(int month, int year) 
+    : month(month), year(year){
+    if (month < 1 || month > 12)
+        throw std::invalid_argument("Invalid month");
+
+    if (year < 0)
+        throw std::invalid_argument("Invalid year");
+}
+
+void Month::addRevenue(const Revenue& revenue) {
+    revenues.push_back(revenue);
+}
+
+Money Month::totalRevenue() const {
+    Money total(0);
+    for(const auto& r : revenues) {
+        total = total + r.getAmount();
+    }
+    return total;
+}
+
+Money Month::totalExpenses() const {
+    Money total(0);
+    for(const auto& e : revenues) {
+        total = total + e.totalExpenses();
+    }
+    return total;
+}
+
+Money Month::balance() const {
+    return totalRevenue() - totalExpenses();
+}
