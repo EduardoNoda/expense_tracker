a2764ca5769aaba70e2e151354533c88e0202ea6
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-12 16:39:29 -0400

    test: mensagem de log para descobrir origem de um bug

--- app/src/main/cpp/core/application/AddRevenueUseCase.cpp
+++ app/src/main/cpp/core/application/AddRevenueUseCase.cpp
@@ -1,10 +1,17 @@
 #include "AddRevenueUseCase.h"
 
+#include <android/log.h>
+#define LOG_TAG "EXPENSE_CORE"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+
 AddRevenueUseCase::AddRevenueUseCase(RevenueRepository& revenueRepository) 
     : revenueRepository(revenueRepository){}
 
 int AddRevenueUseCase::execute(const Money& money, const Date& date) const{
     Revenue revenue(money, date);
+
+    LOGI("AddRevenue is calling\n");
 
     return revenueRepository.save(revenue);
 }
