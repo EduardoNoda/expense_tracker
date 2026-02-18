c7ade6a55a94c0e9357f4901ad1ab4216e22be25
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-18 17:20:46 -0400

    docs: gerenciamento de dependências gradle para construção de interface e experiencia do usuário

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.android.application)
+    // REMOVIDO: O erro diz que o Kotlin JÁ ESTÁ aplicado. Não precisamos aplicar de novo.
+    // id("org.jetbrains.kotlin.android")
+
+    // OBRIGATÓRIO: O compilador do Compose para Kotlin 2.0+
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "br.com.expensetracker"
-
     compileSdk = 36
 
     defaultConfig {
         applicationId = "br.com.expensetracker"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -27,9 +36,20 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 
     externalNativeBuild {
@@ -38,14 +58,35 @@ android {
         }
     }
 
-    ndkVersion = "29.0.14206865"
+    ndkVersion = "26.1.10909125"
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // COMPOSE
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+}
+
+// Configuração do compilador Kotlin
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
