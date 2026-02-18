d90062cab7c9d0b4899bffb3cb0254072cc9f552
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-18 05:13:30 -0400

    fix: separado funcoes SQL e adicionado logs para descobrir qual schema estava sendo aplicado, DROP TABLES para aplicar mudancas forcadas no schema, para MVP mas em producao nao ideal

--- app/src/main/cpp/bridge/core_bridge.cpp
+++ app/src/main/cpp/bridge/core_bridge.cpp
@@ -2,8 +2,10 @@
 #include "../core/infrastructure/Database.h"
 #include "../core/repository/SQLiteExpenseRepository.h"
 #include "../core/repository/SQLiteRevenueRepository.h"
+#include "../core/repository/SQLitePaymentMethodRepository.h"
 #include "../core/application/GetMonthlySummaryUseCase.h"
 #include "../core/application/AddRevenueUseCase.h"
+#include "../core/application/AddExpenseUseCase.h"
 #include <android/log.h>
 #include <android/log.h>
 #include <sstream>
@@ -14,8 +16,10 @@
 static Database* database = nullptr;
 static SQLiteExpenseRepository* expenseRepository = nullptr;
 static SQLiteRevenueRepository* revenueRepository = nullptr;
+static SQLitePaymentMethodRepository* paymentMethodRepository = nullptr;
 static GetMonthlySummaryUseCase* summaryUseCase = nullptr;
 static AddRevenueUseCase* addRevenue = nullptr;
+static AddExpenseUseCase* addExpenseUseCase = nullptr;
 
 extern "C"
 JNIEXPORT jlongArray JNICALL
@@ -38,35 +42,45 @@ Java_br_com_expensetracker_bridge_CoreBridge_getMonthSummary(JNIEnv *env, jobjec
     return array;
 }
 static void ensureSchema(Database& db) {
-
     db.exec("PRAGMA foreign_keys = ON;");
 
     db.exec(R"(
+    DROP TABLE expenses;
+    DROP TABLE payment_methods;
+    )");
 
+    db.exec(R"(
     CREATE TABLE IF NOT EXISTS categories (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE
     );
+    )");
 
+    db.exec(R"(
     CREATE TABLE IF NOT EXISTS payment_methods (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE,
-        type TEXT NOT NULL CHECK (type IN ('DEBIT','CREDIT','PIX','CASH')),
-        closing_day INTEGER CHECK (closing_day BETWEEN 1 AND 31)
+        type TEXT NOT NULL CHECK (type IN ('IMMEDIATE','CREDIT')),
+        closing_day INTEGER CHECK (closing_day IS NULL OR closing_day BETWEEN 1 AND 31)
+        due_day INTEGER CHECK (due_day IS NULL OR due_day BETWEEN 1 AND 31)
     );
+    )");
 
+    db.exec(R"(
     CREATE TABLE IF NOT EXISTS revenues (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         amount_cents INTEGER NOT NULL CHECK (amount_cents > 0),
         date TEXT NOT NULL,
         created_at TEXT NOT NULL DEFAULT (datetime('now'))
     );
+    )");
 
+    db.exec(R"(
     CREATE TABLE IF NOT EXISTS expenses (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         revenue_id INTEGER NOT NULL,
         amount_cents INTEGER NOT NULL CHECK (amount_cents > 0),
         date TEXT NOT NULL,
+        impact_date TEXT NOT NULL,
         category_id INTEGER NOT NULL,
         payment_method_id INTEGER NOT NULL,
         created_at TEXT NOT NULL DEFAULT (datetime('now')),
@@ -75,8 +89,34 @@ static void ensureSchema(Database& db) {
         FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT,
         FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id) ON DELETE RESTRICT
     );
-
     )");
+
+    db.exec(R"(
+    INSERT OR IGNORE INTO categories (id, name) VALUES (1, 'Geral');
+    )");
+
+    db.exec(R"(
+    INSERT OR IGNORE INTO payment_methods(name, type) VALUES ('Dinheiro','IMMEDIATE')
+    )");
+
+    sqlite3_stmt* stmt;
+    sqlite3_prepare_v2(db.get(), "SELECT COUNT(*) FROM payment_methods;", -1, &stmt, nullptr);
+    sqlite3_step(stmt);
+    int count = sqlite3_column_int(stmt, 0);
+    sqlite3_finalize(stmt);
+
+    LOGI("Payment methods count after ensureSchema: %d", count);
+
+    sqlite3_prepare_v2(db.get(),
+                       "SELECT sql FROM sqlite_master WHERE name='payment_methods';",
+                       -1, &stmt, nullptr);
+
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        const char* schema = (const char*)sqlite3_column_text(stmt, 0);
+        LOGI("PAYMENT_METHODS SCHEMA: %s", schema);
+    }
+    sqlite3_finalize(stmt);
+
 }
 
 extern "C"
@@ -92,14 +132,15 @@ Java_br_com_expensetracker_bridge_CoreBridge_initDatabase(
 
         database = new Database(dbPath);
 
-        LOGI("Database path: %s", dbPath);
-
+        LOGI("DB PATH: %s", dbPath);
         ensureSchema(*database);
 
         expenseRepository = new SQLiteExpenseRepository(*database);
         revenueRepository = new SQLiteRevenueRepository(*database);
+        paymentMethodRepository = new SQLitePaymentMethodRepository(*database);
         summaryUseCase = new GetMonthlySummaryUseCase(*expenseRepository, *revenueRepository);
         addRevenue = new AddRevenueUseCase(*revenueRepository);
+        addExpenseUseCase = new AddExpenseUseCase(*expenseRepository, *revenueRepository, *paymentMethodRepository);
 
         env->ReleaseStringUTFChars(path, dbPath);
 
@@ -145,4 +186,30 @@ Java_br_com_expensetracker_bridge_CoreBridge_getRevenuesForMonth(JNIEnv *env, jo
                                                                  jint month, jint year) {
     std::string result = getRevenuesForMonth(month, year);
     return env->NewStringUTF(result.c_str());
+}
+void addExpenseToRevenue(int revenueId, Money amount, int day, int month, int year){
+    Money money(amount);
+    Date date(day, month, year);
+
+    int categoryId = 1;      // fixo por enquanto
+    int paymentMethodId = 1; // fixo por enquanto
+
+    addExpenseUseCase->execute(
+            revenueId,
+            money,
+            date,
+            categoryId,
+            paymentMethodId
+    );
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_br_com_expensetracker_bridge_CoreBridge_addExpenseToRevenue(JNIEnv *env, jobject thiz,
+                                                                 jint revenue_id, jlong amount,
+                                                                 jint day, jint month, jint year) {
+    try {
+        addExpenseToRevenue(revenue_id, amount, day, month, year);
+    }catch (const std::exception& e){
+        __android_log_print(ANDROID_LOG_ERROR, "CoreBridge", "Error: %s", e.what());
+    }
 }
