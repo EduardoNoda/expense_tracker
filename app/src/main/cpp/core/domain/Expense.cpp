501d58fa2a125fc32a7102f1b32507a54be2cf56
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-18 17:27:51 -0400

    feat: adicionado e implementado metodos para relacionar receitas e seus respectivos gastos via ID e operar regras de crédito de acordo com o mês a ser impactado

--- app/src/main/cpp/core/domain/Expense.cpp
+++ app/src/main/cpp/core/domain/Expense.cpp
@@ -2,13 +2,17 @@
 #include <stdexcept>
 
 Expense::Expense(
+    int revenueId,
     Money amount,
     Date date,
+    Date impactDate,
     int categoryId,
     int paymentMethodId
 )
-    : amount(amount),
+    : revenueId(revenueId),
+      amount(amount),
       date(date),
+      impactDate(impactDate),
       categoryId(categoryId),
       paymentMethodId(paymentMethodId)
 {
@@ -19,7 +23,9 @@ Expense::Expense(
         throw std::invalid_argument("Invalid payment method id");
 }
 
+const int& Expense::getRevenueId() const { return revenueId; }
 const Money& Expense::getAmount() const { return amount; }
 const Date& Expense::getDate() const { return date; }
+const Date& Expense::getImpactDate() const { return impactDate; }
 int Expense::getCategoryId() const { return categoryId; }
 int Expense::getPaymentMethodId() const { return paymentMethodId; }
--- app/src/main/cpp/core/domain/Expense.h
+++ app/src/main/cpp/core/domain/Expense.h
@@ -6,21 +6,27 @@
 class Expense {
 public:
     Expense(
+        int revenueId,
         Money amount,
         Date date,
+        Date impactDate,
         int categoryId,
         int paymentMethodId
     );
 
+    const int& getRevenueId() const;
     const Money& getAmount() const;
     const Date& getDate() const;
+    const Date& getImpactDate() const;
 
     int getCategoryId() const;
     int getPaymentMethodId() const;
 
 private:
+    int revenueId;
     Money amount;
     Date date;
+    Date impactDate;
     int categoryId;
     int paymentMethodId;
 };
