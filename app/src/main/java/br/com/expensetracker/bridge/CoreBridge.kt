6a8183402e26803d58cc65d1efdfc4e97b69fe04
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-18 17:41:20 -0400

    feat: instanciado funções a serem expostas pelo JNI

--- app/src/main/java/br/com/expensetracker/bridge/CoreBridge.kt
+++ app/src/main/java/br/com/expensetracker/bridge/CoreBridge.kt
@@ -7,7 +7,20 @@ object CoreBridge {
     }
     external fun initDatabase(path: String)
     external fun getMonthSummary(month: Int, year: Int): LongArray
-    external fun addRevenueUseCase(amount: Long, day: Int, month: Int, year: Int)
+    external fun addRevenueUseCase(name: String, amount: Long, day: Int, month: Int, year: Int)
     external fun getRevenuesForMonth(month: Int, year: Int): String
-    external fun addExpenseToRevenue(revenueId: Int, amount: Long, day: Int, month: Int, year: Int)
+    external fun addExpenseToRevenue(
+        revenueId: Int,
+        amount: Long,
+        day: Int,
+        month: Int,
+        year: Int,
+        categoryId: Int,
+        paymentMethodId: Int,
+        installments: Int
+    )
+
+    external fun getAllCategories(): String
+    external fun getPaymentMethods(): String
+    external fun addPaymentMethod(name: String, closingDay: Int, dueDay: Int)
 }
