8f7c3f0dd040a9d22ec1773e61c5cd40fbff9c1c
Eduardo Faleiros Noda <noda.faleiros.eduardo@gmail.com>
2026-02-18 17:23:24 -0400

    feat: implementado função para atribuir nome à receitas e captar gastos especificos atribuidos a suas respectivas receitas

--- app/src/main/cpp/core/domain/Revenue.cpp
+++ app/src/main/cpp/core/domain/Revenue.cpp
@@ -3,31 +3,37 @@
 #include "Money.h"
 #include <stdexcept>
 
-Revenue::Revenue(Money amount, Date date) 
-    : id(0), amount(amount), date(date){}
+Revenue::Revenue(std::string name, Money amount, Date date)
+        : id(0), name(name), amount(amount), date(date) {}
+
+std::string Revenue::getName() const { return name; }
 
 void Revenue::setId(int id) {
-    if(id <= 0)
+    if (id <= 0)
         throw new std::invalid_argument("Id deve ser positivo\n");
     this->id = id;
 }
 
-int Revenue::getId() const {return id;}
-const Money& Revenue::getAmount() const {return amount;}
-const Date& Revenue::getDate() const {return date;}
+int Revenue::getId() const { return id; }
 
-void Revenue::addExpense(const Expense& expense) {
+const Money &Revenue::getAmount() const { return amount; }
+
+const Date &Revenue::getDate() const { return date; }
+
+const std::vector<Expense> &Revenue::getExpenses() const { return expenses; }
+
+void Revenue::addExpense(const Expense &expense) {
     expenses.push_back(expense);
 }
 
-Money Revenue::totalExpenses() const{
+Money Revenue::totalExpenses() const {
     Money total(0);
 
-    for(const auto& e : expenses) total = total + e.getAmount();
+    for (const auto &e: expenses) total = total + e.getAmount();
 
     return total;
 }
 
-Money Revenue::remaining() const{
+Money Revenue::remaining() const {
     return amount - totalExpenses();
 }
--- app/src/main/cpp/core/domain/Revenue.h
+++ app/src/main/cpp/core/domain/Revenue.h
@@ -7,13 +7,14 @@
 
 class Revenue {
 public:
-    Revenue (Money amaout, Date date);
+    Revenue (std::string name, Money amaout, Date date);
+    std::string getName() const;
 
     int getId() const;
     void setId(int id);
     const Money& getAmount() const;
     const Date& getDate() const;
-
+    const std::vector<Expense> & getExpenses() const;
     void addExpense(const Expense&);
 
     Money totalExpenses() const;
@@ -21,8 +22,8 @@ public:
 
 private:
     int id;
+    std::string name;
     Money amount;
     Date date;
-
     std::vector<Expense> expenses;
 };
